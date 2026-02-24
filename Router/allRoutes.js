d901577d9f4bea897e1bf9365e4b29a22e5343ac
AlexJog <alexdu6610@gmail.com>
2026-02-24 11:29:39 +0100

    Ajout système routage

---
+++ Router/allRoutes.js
@@ -0,0 +1,9 @@
+import Route from "./Route";
+
+// Définir ici vos routes
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html"),];
+    // ajout de nouvelle page ici a la suite avec la meme config
+
+// Le titre s'affiche comme ceci : Route.titre - websitename
+export const websiteName = "Quai Antique";
