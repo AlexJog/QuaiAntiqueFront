133c9f7ebaac7e6b3c6fe02561e87c122e420eaa
AlexJog <alexdu6610@gmail.com>
2026-02-25 12:06:20 +0100

    add signin & signup

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,7 +3,10 @@ import Route from "./Route.js";
 // Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/galerie", "La galerie", "/pages/galerie.html"),];
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/signin", "Connexion", "/pages/signin.html"),
+    new Route("/signup", "Inscription", "/pages/signup.html"),
+];
     // ajout de nouvelle page ici a la suite avec la meme config
 
 // Le titre s'affiche comme ceci : Route.titre - websitename
