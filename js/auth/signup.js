45d0fec95d056317031406660c65e4c05fd60819
AlexJog <alexdu6610@gmail.com>
2026-03-03 10:39:26 +0100

    Eviter faille XSS injection html

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -76,14 +76,14 @@ function validateMail(input) {
 }
 
 function validateRequired(input) {
-    if(input.value != '') {
-        input.classList.add("is-valid");
-        input.classList.remove("is-invalid");
-        return true;
-    } else {
+    if(input.value == '') {
         input.classList.remove("is-valid");
         input.classList.add("is-invalid");
         return false;
+    } else {
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+        return true;
     }
 }
 
